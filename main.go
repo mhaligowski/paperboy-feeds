012cadf304c98bb0658c67f6a71187ab47ad6e3e
Mateusz Haligowski <mhaligowski@gmail.com>
2016-12-30 21:43:58 -0800

    Add CORS handler for Location header

--- main.go
+++ main.go
@@ -12,17 +12,20 @@ func Run() {
 	dao := datastoreFeedsDao{}
 	router := mux.NewRouter()
 
-	router.Handle("/feeds", cors.Default().Handler(getFeedsHandler{dao})).
+	corsHandler := cors.New(cors.Options{
+		AllowedHeaders:{"Location"},
+	})
+	router.Handle("/feeds", corsHandler(getFeedsHandler{dao})).
 		Methods(http.MethodGet)
 
-	getHandler := cors.Default().Handler(getFeedHandler{dao})
+	getHandler := corsHandler(getFeedHandler{dao})
 	router.Handle("/feeds/{feedId}", getHandler).
 		Methods(http.MethodGet)
 
-	postHandler := cors.Default().Handler(postHttpHandler{appengine.NewContext, fetchRssWithUrlFetch, dao})
+	postHandler := corsHandler(postHttpHandler{appengine.NewContext, fetchRssWithUrlFetch, dao})
 	router.Handle("/feeds", postHandler).
 		Methods(http.MethodPost).
 		Headers("Content-Type", "application/json")
 
-	http.Handle("/", cors.Default().Handler(router))
+	http.Handle("/", corsHandler(router))
 }
